13eeb58abff87b9e9a68e86c8cae6195a6e00c89
Tux-Tn <sarhanaissi@gmail.com>
2015-05-20 12:15:04 +0100

    Add database install script

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+$dsn = 'mysql:host=127.0.0.1;dbname=vod';
+$username = '';
+$dbpassword = '';
+$options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+);
+?>
---
+++ install.php
@@ -0,0 +1,22 @@
+<?php
+require_once "connection.php";
+$dbh = new PDO($dsn, $username, $dbpassword, $options);
+$query = "CREATE TABLE IF NOT EXISTS `video` (
+  `id` int(10) unsigned NOT NULL,
+  `url` varchar(256) DEFAULT NULL,
+  `title` varchar(256) DEFAULT NULL,
+  `views` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `speaker` varchar(128) DEFAULT NULL,
+  `thumbnail` varchar(256) DEFAULT NULL,
+  `presentation` varchar(256) DEFAULT NULL,
+  `day` smallint(5) unsigned DEFAULT NULL,
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `starttime` smallint(5) unsigned DEFAULT '0',
+  `endtime` smallint(5) unsigned DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8";
+$st = $dbh->prepare($query);
+$st->execute();
+if($st->errorCode() != "0000")
+  echo "Error : " . $st->errorInfo()[2] ;
+else echo "Done";
+?>
